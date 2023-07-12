09b4c64cee6d6c235245aca0382c6bef15c6c1ce
ququzone <xueping.yang@gmail.com>
2023-07-12 14:45:34 +0800

    add mgland nft

---
+++ src/MgLandNFT.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MgLandNFT is ERC721 {
+    event Mint(address indexed account, uint256 indexed tokenId);
+
+    uint256 public immutable startTime;
+    uint256 public immutable endTime;
+    uint256 public nextTokenId;
+    uint256 public immutable price = 299 ether;
+    address public immutable feeCollector;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _startTime,
+        address _feeCollector
+    ) ERC721(_name, _symbol) {
+        startTime = _startTime;
+        endTime = _startTime + 5 days;
+        feeCollector = _feeCollector;
+    }
+
+    function mint() external payable {
+        require(startTime <= block.timestamp && endTime >= block.timestamp, "sale end");
+        require(nextTokenId < 1000, "sale out");
+        require(msg.value >= price, "invalid fee");
+
+        payable(feeCollector).transfer(msg.value);
+        _mint(msg.sender, ++nextTokenId);
+        emit Mint(msg.sender, nextTokenId);
+    }
+
+    function tokenURI(uint256) public view virtual override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        abi.encodePacked(
+                            '{"name": "',
+                            name(),
+                            '", "',
+                            'description": "MgLand * Mimo NFT", "',
+                            'image":"https://dist.mg.land/nft/spaceship/mimo/base.png"',
+                            '}'
+                        )
+                    )
+                )
+            );
+    }
+}
